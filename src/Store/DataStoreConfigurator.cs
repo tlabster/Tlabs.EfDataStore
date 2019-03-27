a602f7a0da8505c0924aaa4a014619b4b9e368f7
Christian Jäckel <Christian Jckel@DESKTOP-5ITGKPN.POL>
2019-03-27 18:21:08 +0100

    IDStoreConfigModel

---
+++ src/Store/DataStoreConfigurator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+using Tlabs.Config;
+
+namespace Tlabs.Data.Store {
+
+  //TODO: Move this into Tlabs.Data
+  ///<summary><see cref="IDataStore"/> configurator.</summary>
+  public class DataStoreConfigurator<T> : IConfigurator<IServiceCollection> where T : class, IDStoreCtxConfigurator {
+
+    ///<inherit/>
+    public void AddTo(IServiceCollection services, IConfiguration cfg) {
+      var log= Tlabs.App.Logger<DataStoreConfigurator<T>>();
+
+      services.AddSingleton<T>();
+
+      services.AddDbContext<DStoreContext<T>>(opt => {
+        if (!opt.IsConfigured) throw new InvalidOperationException("OptionBuilder must be already configured in service provider...");
+      }, ServiceLifetime.Scoped, ServiceLifetime.Singleton);
+      log.LogDebug("{ctx} added to service colletion.", nameof(DStoreContext<T>));
+
+      services.AddScoped<IDataStore, EfDataStore<DStoreContext<T>>>();
+      log.LogDebug("{dstore} added to service colletion.", nameof(EfDataStore<DStoreContext<T>>));
+    }
+
+  }
+
+}
